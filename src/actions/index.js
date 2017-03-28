2ef3fe6d3e234719153108deb3b4ec3c1961ec13
leelorz6 <leelorz6@gmail.com>
2017-03-28 16:17:42 +0700

    add reducer_weather

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '2b5abf067b6f226f377389a3a0d7d864';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
